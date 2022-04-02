a016de8fc0cb0598ea7d8cce76bae5dd6f54bbdb
Adriana <59716935+adrianalpzs@users.noreply.github.com>
2022-04-01 19:31:00 -0600

    si

---
+++ AZUL/src/main/java/com/itesm/azul/Models/Persona.java
@@ -0,0 +1,26 @@
+package com.itesm.azul.Models;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+
+
+
+}
